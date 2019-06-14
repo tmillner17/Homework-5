f49fa52ac7dc9bfcb1da1aa0072330c323130c03
tyler <tmillner17@gmail.com>
2019-06-13 19:21:54 -0600

    more progress made on my homework

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
 let answers = ["Python", "Michael Jordan", "1998", "Reticulated Python", "Komodo Dragon"]
 let time = 45
 let timerId = setInterval(myTimer, 1000)
-let correctAnswers = 0 
+let correctAnswers = 0
 
-function doneClick(){
+function doneClick() {
     let userAnswers = $("input:checked");
-    for (var x = 0; x < userAnswers.length; x++){
-        let curQuestion = $(userAnswers[x]).attr("data-question");
-        let curAnswer = $(userAnswers[x]).val();
-        console.log( curAnswer === answers[curQuestion]);
-        if (curAnswer === curQuestion){
+    userAnswers.each(function (x, cur) {
+        let curQuestion = $(cur).attr("data-question");
+        let curAnswer = $(cur).val();
+        console.log(curAnswer, answers[curQuestion]);
+        if (curAnswer === answers[curQuestion]) {
             correctAnswers++
-            $("#correct-answers").text("Correct Answers" + correctAnswers)
+            $("#correct-answers").text("Correct Answers:" + correctAnswers + '/' + answers.length)
         }
-    }
+    });
 }
 
 $("#done").on("click", doneClick)
